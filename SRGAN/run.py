0cc64e061959e76278376805d61fa0020ebba12d
Aurelio Amerio <aurelio.amerio@edu.unito.it>
2019-06-23 16:08:32 +0200

    added git ignore

--- SRGAN/run.py
+++ SRGAN/run.py
@@ -12,223 +12,226 @@ from keras.layers.convolutional import Conv2D, UpSampling2D
 from keras.models import Model
 from keras.optimizers import Adam
 from scipy.misc import imread, imresize
+#%%
+class SRGAN:
+    def __init__(self):
+        return
+
+    def residual_block(self, x):
+        """
+        Residual block
+        """
+        filters = [64, 64]
+        kernel_size = 3
+        strides = 1
+        padding = "same"
+        momentum = 0.8
+        activation = "relu"
+
+        res = Conv2D(filters=filters[0], kernel_size=kernel_size, strides=strides, padding=padding)(x)
+        res = Activation(activation=activation)(res)
+        res = BatchNormalization(momentum=momentum)(res)
+
+        res = Conv2D(filters=filters[1], kernel_size=kernel_size, strides=strides, padding=padding)(res)
+        res = BatchNormalization(momentum=momentum)(res)
+
+        # Add res and x
+        res = Add()([res, x])
+        return res
 
 
-def residual_block(x):
-    """
-    Residual block
-    """
-    filters = [64, 64]
-    kernel_size = 3
-    strides = 1
-    padding = "same"
-    momentum = 0.8
-    activation = "relu"
+    def build_generator(self):
+        """
+        Create a generator network using the hyperparameter values defined below
+        :return:
+        """
+        residual_blocks = 16
+        momentum = 0.8
+        input_shape = (64, 64, 3)
 
-    res = Conv2D(filters=filters[0], kernel_size=kernel_size, strides=strides, padding=padding)(x)
-    res = Activation(activation=activation)(res)
-    res = BatchNormalization(momentum=momentum)(res)
+        # Input Layer of the generator network
+        input_layer = Input(shape=input_shape)
 
-    res = Conv2D(filters=filters[1], kernel_size=kernel_size, strides=strides, padding=padding)(res)
-    res = BatchNormalization(momentum=momentum)(res)
+        # Add the pre-residual block
+        gen1 = Conv2D(filters=64, kernel_size=9, strides=1, padding='same', activation='relu')(input_layer)
 
-    # Add res and x
-    res = Add()([res, x])
-    return res
+        # Add 16 residual blocks
+        res = self.residual_block(gen1)
+        for i in range(residual_blocks - 1):
+            res = self.residual_block(res)
 
+        # Add the post-residual block
+        gen2 = Conv2D(filters=64, kernel_size=3, strides=1, padding='same')(res)
+        gen2 = BatchNormalization(momentum=momentum)(gen2)
 
-def build_generator():
-    """
-    Create a generator network using the hyperparameter values defined below
-    :return:
-    """
-    residual_blocks = 16
-    momentum = 0.8
-    input_shape = (64, 64, 3)
+        # Take the sum of the output from the pre-residual block(gen1) and the post-residual block(gen2)
+        gen3 = Add()([gen2, gen1])
 
-    # Input Layer of the generator network
-    input_layer = Input(shape=input_shape)
+        # Add an upsampling block
+        gen4 = UpSampling2D(size=2)(gen3)
+        gen4 = Conv2D(filters=256, kernel_size=3, strides=1, padding='same')(gen4)
+        gen4 = Activation('relu')(gen4)
 
-    # Add the pre-residual block
-    gen1 = Conv2D(filters=64, kernel_size=9, strides=1, padding='same', activation='relu')(input_layer)
+        # Add another upsampling block
+        gen5 = UpSampling2D(size=2)(gen4)
+        gen5 = Conv2D(filters=256, kernel_size=3, strides=1, padding='same')(gen5)
+        gen5 = Activation('relu')(gen5)
 
-    # Add 16 residual blocks
-    res = residual_block(gen1)
-    for i in range(residual_blocks - 1):
-        res = residual_block(res)
+        # Output convolution layer
+        gen6 = Conv2D(filters=3, kernel_size=9, strides=1, padding='same')(gen5)
+        output = Activation('tanh')(gen6)
 
-    # Add the post-residual block
-    gen2 = Conv2D(filters=64, kernel_size=3, strides=1, padding='same')(res)
-    gen2 = BatchNormalization(momentum=momentum)(gen2)
-
-    # Take the sum of the output from the pre-residual block(gen1) and the post-residual block(gen2)
-    gen3 = Add()([gen2, gen1])
-
-    # Add an upsampling block
-    gen4 = UpSampling2D(size=2)(gen3)
-    gen4 = Conv2D(filters=256, kernel_size=3, strides=1, padding='same')(gen4)
-    gen4 = Activation('relu')(gen4)
-
-    # Add another upsampling block
-    gen5 = UpSampling2D(size=2)(gen4)
-    gen5 = Conv2D(filters=256, kernel_size=3, strides=1, padding='same')(gen5)
-    gen5 = Activation('relu')(gen5)
-
-    # Output convolution layer
-    gen6 = Conv2D(filters=3, kernel_size=9, strides=1, padding='same')(gen5)
-    output = Activation('tanh')(gen6)
-
-    # Keras model
-    model = Model(inputs=[input_layer], outputs=[output], name='generator')
-    return model
+        # Keras model
+        model = Model(inputs=[input_layer], outputs=[output], name='generator')
+        return model
 
 
-def build_discriminator():
-    """
-    Create a discriminator network using the hyperparameter values defined below
-    :return:
-    """
-    leakyrelu_alpha = 0.2
-    momentum = 0.8
-    input_shape = (256, 256, 3)
+    def build_discriminator(self):
+        """
+        Create a discriminator network using the hyperparameter values defined below
+        :return:
+        """
+        leakyrelu_alpha = 0.2
+        momentum = 0.8
+        input_shape = (256, 256, 3)
 
-    input_layer = Input(shape=input_shape)
+        input_layer = Input(shape=input_shape)
 
-    # Add the first convolution block
-    dis1 = Conv2D(filters=64, kernel_size=3, strides=1, padding='same')(input_layer)
-    dis1 = LeakyReLU(alpha=leakyrelu_alpha)(dis1)
+        # Add the first convolution block
+        dis1 = Conv2D(filters=64, kernel_size=3, strides=1, padding='same')(input_layer)
+        dis1 = LeakyReLU(alpha=leakyrelu_alpha)(dis1)
 
-    # Add the 2nd convolution block
-    dis2 = Conv2D(filters=64, kernel_size=3, strides=2, padding='same')(dis1)
-    dis2 = LeakyReLU(alpha=leakyrelu_alpha)(dis2)
-    dis2 = BatchNormalization(momentum=momentum)(dis2)
+        # Add the 2nd convolution block
+        dis2 = Conv2D(filters=64, kernel_size=3, strides=2, padding='same')(dis1)
+        dis2 = LeakyReLU(alpha=leakyrelu_alpha)(dis2)
+        dis2 = BatchNormalization(momentum=momentum)(dis2)
 
-    # Add the third convolution block
-    dis3 = Conv2D(filters=128, kernel_size=3, strides=1, padding='same')(dis2)
-    dis3 = LeakyReLU(alpha=leakyrelu_alpha)(dis3)
-    dis3 = BatchNormalization(momentum=momentum)(dis3)
+        # Add the third convolution block
+        dis3 = Conv2D(filters=128, kernel_size=3, strides=1, padding='same')(dis2)
+        dis3 = LeakyReLU(alpha=leakyrelu_alpha)(dis3)
+        dis3 = BatchNormalization(momentum=momentum)(dis3)
 
-    # Add the fourth convolution block
-    dis4 = Conv2D(filters=128, kernel_size=3, strides=2, padding='same')(dis3)
-    dis4 = LeakyReLU(alpha=leakyrelu_alpha)(dis4)
-    dis4 = BatchNormalization(momentum=0.8)(dis4)
+        # Add the fourth convolution block
+        dis4 = Conv2D(filters=128, kernel_size=3, strides=2, padding='same')(dis3)
+        dis4 = LeakyReLU(alpha=leakyrelu_alpha)(dis4)
+        dis4 = BatchNormalization(momentum=0.8)(dis4)
 
-    # Add the fifth convolution block
-    dis5 = Conv2D(256, kernel_size=3, strides=1, padding='same')(dis4)
-    dis5 = LeakyReLU(alpha=leakyrelu_alpha)(dis5)
-    dis5 = BatchNormalization(momentum=momentum)(dis5)
+        # Add the fifth convolution block
+        dis5 = Conv2D(256, kernel_size=3, strides=1, padding='same')(dis4)
+        dis5 = LeakyReLU(alpha=leakyrelu_alpha)(dis5)
+        dis5 = BatchNormalization(momentum=momentum)(dis5)
 
-    # Add the sixth convolution block
-    dis6 = Conv2D(filters=256, kernel_size=3, strides=2, padding='same')(dis5)
-    dis6 = LeakyReLU(alpha=leakyrelu_alpha)(dis6)
-    dis6 = BatchNormalization(momentum=momentum)(dis6)
+        # Add the sixth convolution block
+        dis6 = Conv2D(filters=256, kernel_size=3, strides=2, padding='same')(dis5)
+        dis6 = LeakyReLU(alpha=leakyrelu_alpha)(dis6)
+        dis6 = BatchNormalization(momentum=momentum)(dis6)
 
-    # Add the seventh convolution block
-    dis7 = Conv2D(filters=512, kernel_size=3, strides=1, padding='same')(dis6)
-    dis7 = LeakyReLU(alpha=leakyrelu_alpha)(dis7)
-    dis7 = BatchNormalization(momentum=momentum)(dis7)
+        # Add the seventh convolution block
+        dis7 = Conv2D(filters=512, kernel_size=3, strides=1, padding='same')(dis6)
+        dis7 = LeakyReLU(alpha=leakyrelu_alpha)(dis7)
+        dis7 = BatchNormalization(momentum=momentum)(dis7)
 
-    # Add the eight convolution block
-    dis8 = Conv2D(filters=512, kernel_size=3, strides=2, padding='same')(dis7)
-    dis8 = LeakyReLU(alpha=leakyrelu_alpha)(dis8)
-    dis8 = BatchNormalization(momentum=momentum)(dis8)
+        # Add the eight convolution block
+        dis8 = Conv2D(filters=512, kernel_size=3, strides=2, padding='same')(dis7)
+        dis8 = LeakyReLU(alpha=leakyrelu_alpha)(dis8)
+        dis8 = BatchNormalization(momentum=momentum)(dis8)
 
-    # Add a dense layer
-    dis9 = Dense(units=1024)(dis8)
-    dis9 = LeakyReLU(alpha=0.2)(dis9)
+        # Add a dense layer
+        dis9 = Dense(units=1024)(dis8)
+        dis9 = LeakyReLU(alpha=0.2)(dis9)
 
-    # Last dense layer - for classification
-    output = Dense(units=1, activation='sigmoid')(dis9)
+        # Last dense layer - for classification
+        output = Dense(units=1, activation='sigmoid')(dis9)
 
-    model = Model(inputs=[input_layer], outputs=[output], name='discriminator')
-    return model
-
-
-def build_vgg():
-    """
-    Build VGG network to extract image features
-    """
-    input_shape = (256, 256, 3)
-
-    # Load a pre-trained VGG19 model trained on 'Imagenet' dataset
-    vgg = VGG19(weights="imagenet")
-    vgg.outputs = [vgg.layers[9].output]
-
-    input_layer = Input(shape=input_shape)
-
-    # Extract features
-    features = vgg(input_layer)
-
-    # Create a Keras model
-    model = Model(inputs=[input_layer], outputs=[features])
-    return model
+        model = Model(inputs=[input_layer], outputs=[output], name='discriminator')
+        return model
 
 
-def sample_images(data_dir, batch_size, high_resolution_shape, low_resolution_shape):
-    # Make a list of all images inside the data directory
-    all_images = glob.glob(data_dir)
+    def build_vgg(self):
+        """
+        Build VGG network to extract image features
+        """
+        input_shape = (256, 256, 3)
 
-    # Choose a random batch of images
-    images_batch = np.random.choice(all_images, size=batch_size)
+        # Load a pre-trained VGG19 model trained on 'Imagenet' dataset
+        vgg = VGG19(weights="imagenet")
+        vgg.outputs = [vgg.layers[9].output]
 
-    low_resolution_images = []
-    high_resolution_images = []
+        input_layer = Input(shape=input_shape)
 
-    for img in images_batch:
-        # Get an ndarray of the current image
-        img1 = imread(img, mode='RGB')
-        img1 = img1.astype(np.float32)
+        # Extract features
+        features = vgg(input_layer)
 
-        # Resize the image
-        img1_high_resolution = imresize(img1, high_resolution_shape)
-        img1_low_resolution = imresize(img1, low_resolution_shape)
-
-        # Do a random horizontal flip
-        if np.random.random() < 0.5:
-            img1_high_resolution = np.fliplr(img1_high_resolution)
-            img1_low_resolution = np.fliplr(img1_low_resolution)
-
-        high_resolution_images.append(img1_high_resolution)
-        low_resolution_images.append(img1_low_resolution)
-
-    # Convert the lists to Numpy NDArrays
-    return np.array(high_resolution_images), np.array(low_resolution_images)
+        # Create a Keras model
+        model = Model(inputs=[input_layer], outputs=[features])
+        return model
 
 
-def save_images(low_resolution_image, original_image, generated_image, path):
-    """
-    Save low-resolution, high-resolution(original) and
-    generated high-resolution images in a single image
-    """
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 3, 1)
-    ax.imshow(low_resolution_image)
-    ax.axis("off")
-    ax.set_title("Low-resolution")
+    def sample_images(self, data_dir, batch_size, high_resolution_shape, low_resolution_shape):
+        # Make a list of all images inside the data directory
+        all_images = glob.glob(data_dir)
 
-    ax = fig.add_subplot(1, 3, 2)
-    ax.imshow(original_image)
-    ax.axis("off")
-    ax.set_title("Original")
+        # Choose a random batch of images
+        images_batch = np.random.choice(all_images, size=batch_size)
 
-    ax = fig.add_subplot(1, 3, 3)
-    ax.imshow(generated_image)
-    ax.axis("off")
-    ax.set_title("Generated")
+        low_resolution_images = []
+        high_resolution_images = []
 
-    plt.savefig(path)
+        for img in images_batch:
+            # Get an ndarray of the current image
+            img1 = imread(img, mode='RGB')
+            img1 = img1.astype(np.float32)
+
+            # Resize the image
+            img1_high_resolution = imresize(img1, high_resolution_shape)
+            img1_low_resolution = imresize(img1, low_resolution_shape)
+
+            # Do a random horizontal flip
+            if np.random.random() < 0.5:
+                img1_high_resolution = np.fliplr(img1_high_resolution)
+                img1_low_resolution = np.fliplr(img1_low_resolution)
+
+            high_resolution_images.append(img1_high_resolution)
+            low_resolution_images.append(img1_low_resolution)
+
+        # Convert the lists to Numpy NDArrays
+        return np.array(high_resolution_images), np.array(low_resolution_images)
 
 
-def write_log(callback, name, value, batch_no):
-    """
-    Write scalars to Tensorboard
-    """
-    summary = tf.Summary()
-    summary_value = summary.value.add()
-    summary_value.simple_value = value
-    summary_value.tag = name
-    callback.writer.add_summary(summary, batch_no)
-    callback.writer.flush()
+    def save_images(self, low_resolution_image, original_image, generated_image, path):
+        """
+        Save low-resolution, high-resolution(original) and
+        generated high-resolution images in a single image
+        """
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 3, 1)
+        ax.imshow(low_resolution_image)
+        ax.axis("off")
+        ax.set_title("Low-resolution")
+
+        ax = fig.add_subplot(1, 3, 2)
+        ax.imshow(original_image)
+        ax.axis("off")
+        ax.set_title("Original")
+
+        ax = fig.add_subplot(1, 3, 3)
+        ax.imshow(generated_image)
+        ax.axis("off")
+        ax.set_title("Generated")
+
+        plt.savefig(path)
+
+
+    def write_log(self, callback, name, value, batch_no):
+        """
+        Write scalars to Tensorboard
+        """
+        summary = tf.Summary()
+        summary_value = summary.value.add()
+        summary_value.simple_value = value
+        summary_value.tag = name
+        callback.writer.add_summary(summary, batch_no)
+        callback.writer.flush()
 
 
 if __name__ == '__main__':
